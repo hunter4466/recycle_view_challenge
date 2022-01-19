e9fbc2259badde3013231affc4805c0eb66de415
Mario Chois <mario.chois.navarro@gmail.com>
2022-01-19 15:06:56 -0500

    Database: Create database to store fetched information from API

--- app/src/main/java/com/ravnnerdery/recyclechallenge/postlist/PostlistAdapter.kt
+++ app/src/main/java/com/ravnnerdery/recyclechallenge/postlist/PostlistAdapter.kt
@@ -1,5 +1,13 @@
 package com.ravnnerdery.recyclechallenge.postlist
 
-class PostlistAdapter {
+import androidx.recyclerview.widget.RecyclerView
 
+class PostlistAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
+    var data = listOf<>()
+
+    override fun getItemCount() = data.size
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+        val item = data[position]
+        holder.textView.text = item.sleepQuality
+    }
 }
